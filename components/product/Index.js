eb45b9de5dd1a35f116a36294136936b698492cf
Ville Viljanen <salt-dev@salt-devs-MacBook-Air-98.local>
2022-11-14 11:11:25 +0100

    test

---
+++ components/product/Index.js
@@ -0,0 +1,16 @@
+function Product({ date, image, title }) {
+    let { file, description } = image
+  
+    return (
+      <div className="product">
+        <img alt={description} src={`https:${file.url}`} />
+        <div className="description">{description}</div>
+        <div className="text">
+          <h2>{title}</h2>
+          <h3>{date.substring(0, 10)}</h3>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Product
