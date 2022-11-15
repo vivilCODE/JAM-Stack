3636f10d4750a169f0a708693d811f1f956374b9
Ville Viljanen <salt-dev@salt-devs-MacBook-Air-98.local>
2022-11-15 15:43:15 +0100

    e

--- pages/api/model.js
+++ pages/api/model.js
@@ -5,7 +5,7 @@ const stockSchema = new mongoose.Schema({
     productId: String,
 })
 
-const uri = process.env.NEXT_PUBLIC_MONGO_ACCESS_KEY;
+const uri = 'mongodb+srv://ville:viljanen@cluster0.cq1qjhv.mongodb.net/?retryWrites=true&w=majority';
 
 const Stock = mongoose.models.Stock || mongoose.model('Stock', stockSchema);
 
