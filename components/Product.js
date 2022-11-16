cd45561ffb65aa079bdb872efecbc68e0a352484
Ville Viljanen <salt-dev@salt-devs-MacBook-Air-98.local>
2022-11-16 13:50:54 +0100

    e

--- components/Product.js
+++ components/Product.js
@@ -1,28 +1,30 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function Product({ product }) {
   const [qty, setQty] = useState();
-
+  console.log(product);
   useEffect(() => {
-
     fetch(`/api/products/${product.fields.id}`)
-      .then(res => res.json()).then(data => setQty(data))
-  }, [])
+      .then(res => res.json())
+      .then(data => setQty(data));
+  }, []);
 
   const src = product.fields.image.fields.file.url;
-  const {title, price} = product.fields;
+  const { title, price } = product.fields;
 
- return (
+  return (
     <div className="product">
-      <img alt={title} src={`https:${src}`} />
+      {/* <img alt={title} src={`https:${src}`} /> */}
+      <video width="400" height='400px' controls>
+        <source src={product.fields.image.fields.file.url} /* type="video/mp4"  *//>
+      </video>
       <div className="text">
         <h2>{title}</h2>
-        <h2>{price}</h2>
+        <h2>Price: € {price}</h2>
         <h2>In stock: {qty}</h2>
       </div>
-    </div> 
+    </div>
   );
- 
 }
- 
+
 export default Product;
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -4,5 +4,8 @@ export default async function handler(req, res) {
     const {id} = req.query
     console.log('ID', id)
     const data =  await Stock.findOne({productId: id})
+    if(!data) {
+        res.send();
+    }
     res.status(200).json(data.quantity);
 }
